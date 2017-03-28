d54b22f210270f9a453085ccf1d999f1da13c52e
Adrian Dominguez <pilon.dev@gmail.com>
2017-03-28 13:50:04 -0600

    se agregan 2 pantallas nuevas con las listas de las nversiones y las que existen en en mercado

---
+++ inversionistas/assets/js/lista-mercados.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('#dataTables-example').DataTable({
+        responsive: true
+    });
+
+    $(".detalle-comprador").click(function(){
+        var nombreClienteTD = $(this).parents('tr').find('td.td-nombre-cliente');
+        var nombreClienteSpan = nombreClienteTD.find('span.nombre-cliente').text();
+        $("#nombre-cliente-h4").text(nombreClienteSpan);
+        $("#client-detail-modal").modal("show");
+
+    });
+
+    $("#eliminar-factura").click(function(){
+        var table = $("#dataTables-example");
+        $("#eliminar-factura-exitosa").slideDown();
+            setTimeout(function(){
+            $("#smallModal").modal("hide");
+            }, 3000)
+        var row = table.find("tr.deleted");
+        row.fadeOut(500);
+    });
+
+    $("#adquirir-facturas").click(function(){
+        $("#autorizar-inversion-modal").modal("show");
+    });
+});
--- proveedores/assets/js/listado-facturas.js
+++ proveedores/assets/js/listado-facturas.js
@@ -9,18 +9,41 @@ $(document).ready(function() {
     });
 
     $('.edit-fact-ico').click(function () {
+        //Metodo para obtener los valores de los campos de fecha de pago y porcentaje
+        //Setea los nuevos valores por los que selecciones y los pone actualizados en la tabla
+
+        $("#edit-modal").modal("show");
+        var dateTD = $(this).parents('tr').find('td.td-fecha-pago-fact');
+        var dateEditableSpan = dateTD.find('span.date-editable');
+        var dateEditable = dateTD.find('span.date-editable').text();
+        $("#datepicker").val(dateEditable);
+        var valorFecha = $("#datepicker").val(dateEditable);
+
         var currentTD = $(this).parents('tr').find('td.td-porcentaje-fact');
         var spanPorcentaje = currentTD.find('span.porcentaje-editable');
-        spanPorcentaje.prop("contenteditable", true);
-        $(spanPorcentaje).keypress(function (event) {
-            return /\d/.test(String.fromCharCode(event.keyCode));
+        var spanPorcentajeValor = spanPorcentaje.text();
+        $("#ceder-porcentaje").val(spanPorcentajeValor);
+        valorPorcentaje = $("#ceder-porcentaje").val(spanPorcentajeValor);
+
+        $("#editar-factura").click(function(){
+
+            $("#actualizacion-factura-exitosa").slideDown();
+            setTimeout(function(){
+                $("#edit-modal").modal("hide");;
+            }, 3000)
+
+            dateEditableSpan.text(valorFecha.val());
+            spanPorcentaje.text(valorPorcentaje.val());
         });
     });
 
     $("#eliminar-factura").click(function(){
         var table = $("#dataTables-example");
+        $("#eliminar-factura-exitosa").slideDown();
+            setTimeout(function(){
+            $("#smallModal").modal("hide");
+            }, 3000)
         var row = table.find("tr.deleted");
-        $("#smallModal").modal("hide");
         row.fadeOut(500);
     });
 });
